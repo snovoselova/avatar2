dca730345093aaef0fe7739fc23d705750bb5eba
Svetlana <novoselovasv88@gmail.com>
2025-09-07 15:36:26 -0300

    Сохранение прогресса.

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,15 @@
 import Swiper from 'swiper';
-import { Navigation, Pagination, Autoplay, Keyboard, A11y } from 'swiper/modules';
+import {Navigation, Pagination, Autoplay, Keyboard, A11y} from 'swiper/modules';
 
 document.addEventListener('DOMContentLoaded', () => {
     new Swiper('.js-swiper', {
         modules: [Navigation, Pagination, Autoplay, Keyboard, A11y],
-        loop: false,
+        loop: true,
         slidesPerView: 1,
-        spaceBetween: 0,
-        autoplay: { delay: 4000, disableOnInteraction: false },
-        pagination: { el: '.swiper-pagination', clickable: true },
-        navigation: { nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev' },
-        keyboard: { enabled: true },
-        a11y: { enabled: true }
+        spaceBetween: 10,
+        pagination: {el: '.swiper-pagination', clickable: true},
+        navigation: {nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev'},
+        keyboard: {enabled: true},
+        a11y: {enabled: true}
     });
 });

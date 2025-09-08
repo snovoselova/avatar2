4f0b7c2ca32c3dcc6275f894d85cb7bf74247bd8
Svetlana <novoselovasv88@gmail.com>
2025-09-08 20:58:13 -0300

    Saving progress.

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,8 @@
 import Swiper from 'swiper';
-import {Navigation, Pagination, Autoplay, Keyboard, A11y} from 'swiper/modules';
 
 document.addEventListener('DOMContentLoaded', () => {
-    new Swiper('.js-swiper', {
-        modules: [Navigation, Pagination, Autoplay, Keyboard, A11y],
-        loop: true,
-        slidesPerView: 1,
-        spaceBetween: 10,
-        pagination: {el: '.swiper-pagination', clickable: true},
-        navigation: {nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev'},
-        keyboard: {enabled: true},
-        a11y: {enabled: true}
+    new Swiper('.common-slider', {
+        loop: false,
+        spaceBetween: 24,
     });
 });

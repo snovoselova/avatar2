8b368819e74fed58d458ef96d01a2ea631764d7a
Svetlana <novoselovasv88@gmail.com>
2025-09-11 16:17:29 -0300

    Toggle для футера.

--- src/js/main.js
+++ src/js/main.js
@@ -5,4 +5,24 @@ document.addEventListener('DOMContentLoaded', () => {
         loop: false,
         spaceBetween: 24,
     });
+
+    // Toggle для футера
+    const footerToggle = document.querySelector('.footer-toggle');
+    const footerLink = document.querySelector('.footer-top .logo');
+    const footerContent = document.querySelector('.footer-content');
+
+    if (footerToggle && footerContent) {
+        footerToggle.addEventListener('click', footerToggleHandler);
+        footerLink.addEventListener('click', footerToggleHandler);
+    }
+
+    function footerToggleHandler(e) {
+        const isExpanded = footerToggle.getAttribute('aria-expanded') === 'true';
+
+        // Переключаем состояние
+        footerToggle.setAttribute('aria-expanded', !isExpanded);
+        footerContent.classList.toggle('hidden');
+
+        e.preventDefault();
+    }
 });

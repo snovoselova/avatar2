74cc2f1397573377e2ad02428b609c1d834157b0
Svetlana <novoselovasv88@gmail.com>
2025-09-10 22:46:00 -0300

    Saving progress.

--- landings.config.js
+++ landings.config.js
@@ -1,6 +1,6 @@
 module.exports = [
     { id: 'landing-a', header: 'header1.png', titleSuffix: 'A' },
-    { id: 'landing-b', header: 'header2.svg', titleSuffix: 'B' },
-    { id: 'landing-c', header: 'header3.svg', titleSuffix: 'C' },
-    { id: 'landing-d', header: 'header4.svg', titleSuffix: 'D' }
+    { id: 'landing-b', header: 'header2.jpg', titleSuffix: 'B' },
+    { id: 'landing-c', header: 'header3.jpg', titleSuffix: 'C' },
+    { id: 'landing-d', header: 'header4.jpg', titleSuffix: 'D' }
 ];

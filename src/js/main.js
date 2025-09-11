4b68f364c8f06fbfce690877a3273867e6279408
Svetlana <novoselovasv88@gmail.com>
2025-09-11 17:39:05 -0300

    Маска телефона.

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,18 @@
 import Swiper from 'swiper';
+import Inputmask from 'inputmask';
 
 document.addEventListener('DOMContentLoaded', () => {
     new Swiper('.common-slider', {
         loop: false,
         spaceBetween: 24,
     });
+
+    // Phone input mask for registration form
+    const phoneInput = document.querySelector('input[type="tel"]');
+    if (phoneInput) {
+        const phoneMask = new Inputmask('(999) 999-99-99');
+        phoneMask.mask(phoneInput);
+    }
 
     // Toggle для футера
     const footerToggle = document.querySelector('.footer-toggle');
